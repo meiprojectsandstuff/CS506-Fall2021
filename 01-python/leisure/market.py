20dd50a639433e23638a6a4f1ab37186bfb27367
Timothy M <timothy@dhcp-wifi-8021x-155-41-15-234.bu.edu>
2021-09-23 15:53:14 -0400

    added market
    removed previous lab commits and added variable floors to market.py

--- 01-python/leisure/market.py
+++ 01-python/leisure/market.py
@@ -1,3 +1,26 @@
-def draw_market():
-    print("market not found")
-    return
+def draw_market(floors):
+    if (floors < 1):
+            return
+    else:
+        print("-----------------------------------------------------------")
+        for i in range(floors-1):
+            print("|                                                         |")
+            print("|                  |              |                       |")
+            print("|            o                          o                 |")
+            print("|             o                        o                  |")
+            print("|              o                      o                   |")
+            print("|               o                    o                    |")
+            print("|                oooooooooooooooooooo                     |")
+            print("|                                                         |")
+        print("|                    HAPPY MARKET                         |")
+        print("|                   WE GOT RATS                           |")
+        print("|              ----------------------                     |")
+        print("|              |         |          |                     |")
+        print("|              |         |          |                     |")
+        print("|              |         |          |                     |")
+        print("|              |         |          |                     |")
+        print("|              |         |          |                     |")
+        print("|              |         |          |                     |")
+        print("|              |         |          |                     |")
+        print("-----------------------------------------------------------")
+        return
